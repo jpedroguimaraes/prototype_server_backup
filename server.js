a67c52a7a3b97976c50361b4d5626b2fe0f52e59
JP Guimaraes <j.pedro.guimaraes@hotmail.com>
2015-11-26 01:34:53 +0000

    Correction

--- server.js
+++ server.js
@@ -1,92 +1,157 @@
 #!/bin/env node
-// You'll see the client-side's output on the console when you run it.
+//  OpenShift sample Node application
+var express = require('express');
+var fs      = require('fs');
 
-var restify = require('restify');
 
-// State
-var next_user_id = 0;
-var users = {};
+/**
+ *  Define the sample application.
+ */
+var SampleApp = function() {
 
-// Server
-var server = restify.createServer({
-  name: 'myapp',
-  version: '1.0.0'
-});
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+    //  Scope.
+    var self = this;
 
-server.get("/", function (req, res, next) {
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(users));
-  return next();
-});
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
 
-server.get('/user/:id', function (req, res, next) {
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(users[parseInt(req.params.id)]));
-  return next();
-});
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-server.post('/user', function (req, res, next) {
-  var user = req.params;
-  user.id = next_user_id++;
-  users[user.id] = user;
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(user));
-  return next();
-});
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        };
+    };
 
-server.put('/user/:id', function (req, res, next) {
-  var user = users[parseInt(req.params.id)];
-  var changes = req.params;
-  delete changes.id;
-  for(var x in changes) {
-    user[x] = changes[x];
-  }
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(user));
-  return next();
-});
 
-server.del('/user/:id', function (req, res, next) {
-  delete users[parseInt(req.params.id)];
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(true));
-  return next();
-});
+    /**
+     *  Populate the cache.
+     */
+    self.populateCache = function() {
+        if (typeof self.zcache === "undefined") {
+            self.zcache = { 'index.html': '' };
+        }
 
-server.listen(80, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
+        //  Local cache for static content.
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
+    };
 
-// Client
-var client = restify.createJsonClient({
-  url: 'http://localhost:80',
-  version: '~1.0'
-});
 
-client.post('/user', { name: "John Doe" }, function (err, req, res, obj) {
-  if(err) console.log("An error ocurred:", err);
-  else console.log('POST    /user   returned: %j', obj);
-  
-  client.get('/user/0', function (err, req, res, obj) {
-    if(err) console.log("An error ocurred:", err);
-    else console.log('GET     /user/0 returned: %j', obj);
-    
-    client.put('/user/0', { country: "USA" }, function (err, req, res, obj) {
-      if(err) console.log("An error ocurred:", err);
-      else console.log('PUT     /user/0 returned: %j', obj);
-      
-      client.del('/user/0', function (err, req, res, obj) {
-        if(err) console.log("An error ocurred:", err);
-        else console.log('DELETE  /user/0 returned: %j', obj);
-        
-        client.get('/', function (err, req, res, obj) {
-          if(err) console.log("An error ocurred:", err);
-          else console.log('GET     /       returned: %j', obj);
+    /**
+     *  Retrieve entry (content) from cache.
+     *  @param {string} key  Key identifying content to retrieve from cache.
+     */
+    self.cache_get = function(key) { return self.zcache[key]; };
+
+
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
+        }
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
         });
-      });
-    });
-  });
-});
+    };
+
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+    self.createRoutes = function() {
+        self.routes = { };
+
+        self.routes['/asciimo'] = function(req, res) {
+            var link = "http://i.imgur.com/kmbjB.png";
+            res.send("<html><body><img src='" + link + "'></body></html>");
+        };
+
+        self.routes['/'] = function(req, res) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(self.cache_get('index.html') );
+        };
+    };
+
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+        self.createRoutes();
+        self.app = express.createServer();
+
+        //  Add handlers for the app (from the routes).
+        for (var r in self.routes) {
+            self.app.get(r, self.routes[r]);
+        }
+    };
+
+
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        self.populateCache();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+    };
+
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+};   /*  Sample Application.  */
+
+
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new SampleApp();
+zapp.initialize();
+zapp.start();
